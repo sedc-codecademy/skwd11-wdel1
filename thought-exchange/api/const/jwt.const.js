3ebd1caf00b42299a953137342895ad3ad100383
borisovski-borche <borisovski.borce@gmail.com>
2023-09-28 20:31:47 +0200

    finished api

--- thought-exchange/api/const/jwt.const.js
+++ thought-exchange/api/const/jwt.const.js
@@ -2,10 +2,20 @@ import jwt from "jsonwebtoken";
 
 export const createAccessToken = userId => {
   return jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "30m",
+    expiresIn: "20s",
   });
 };
 
 export const verifyAccessToken = token => {
   return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+};
+
+export const createRefreshToken = userId => {
+  return jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+export const verifyRefreshToken = token => {
+  return jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
 };
